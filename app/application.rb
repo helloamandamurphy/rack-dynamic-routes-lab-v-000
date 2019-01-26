a90ffb232bffeaa6a4023e16fc58c2c95b5ee10b
Amanda Murphy <helloamandamurphy@gmail.com>
2019-01-26 21:39:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
-      end
+      
+    
+    resp.finish
 end 
